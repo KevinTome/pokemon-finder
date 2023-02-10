0a24a65344eb531887aa5233d9254ca2504f6733
Kevin Tome <k.tome@unifesp.br>
2023-02-10 17:28:09 -0300

    feat: working API & responsive layout

--- pokedex-vue/src/views/HomeView.vue
+++ pokedex-vue/src/views/HomeView.vue
@@ -1,4 +1,46 @@
-<script setup lang="ts">
+<script lang="ts">
+  import axios from 'axios';
+  
+  export default{
+    data(){
+      return{
+        isActive: true,
+        inputInfo: '',
+        pokemonData: '',
+        nameInfo: 'Name',
+        typeInfo: 'Type',
+        imgSrc: '',
+        hpStats: 'HP',
+        atkStats: 'Atk',
+        defStats: 'Def',
+        satkStats: 'SAtk',
+        sdefStats: 'SDef',
+        spdStats: 'Spd'
+      };
+    },
+    methods: {
+      search(e: string) {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${this.inputInfo.toLowerCase()}`)
+        .then((response) => {
+          this.pokemonData = response.data;
+
+          this.isActive = false;
+          this.nameInfo = this.pokemonData.name;
+          this.typeInfo = this.pokemonData.types[0].type.name; //[ERR]more than one type
+          this.imgSrc = this.pokemonData.sprites.front_default;
+          this.hpStats = this.pokemonData.stats[0].base_stat;
+          this.atkStats = this.pokemonData.stats[1].base_stat;
+          this.defStats = this.pokemonData.stats[2].base_stat;
+          this.satkStats = this.pokemonData.stats[3].base_stat;
+          this.sdefStats = this.pokemonData.stats[4].base_stat;
+          this.spdStats = this.pokemonData.stats[5].base_stat;
+        })
+        .catch((err) => {
+          alert("There's not a Pokemon with that name, try again!")
+        });
+      }
+    }
+  };
 
 </script>
 
@@ -8,45 +50,45 @@
       <div class="form">
         <h3>Search your Pokemon here:</h3>
         <div class="form-input-container">
-          <input type="text" placeholder="type any pokemon name" id="pokemon-name">
-          <button class="search">
+          <input type="text" v-model="inputInfo" placeholder="type any pokemon name" id="pokemon-name">
+          <button class="search" @click="search(inputInfo)">
             <i class="fa-solid fa-magnifying-glass input-button"></i>
           </button>
         </div>
       </div>
-      <div class="poke-data">
+      <div v-bind:class="{hide: isActive}" class="poke-data">
         <div class="poke-title-info">
-          <h2 class="hovertext poke-name" data-hover="Name">Name</h2>
-          <p class="hovertext type" data-hover="Type">Type</p>  
+          <h2 class="hovertext poke-name" data-hover="Name">{{ nameInfo }}</h2>
+          <p class="hovertext type" data-hover="Type">{{ typeInfo }}</p>  
         </div>
         <div class="poke-sprites">
-          <!-- <img src="" alt="Image" class="poke-img"> -->
-          <p class="dot"></p>
+          <img :src="imgSrc" alt="Image" class="poke-img">
         </div>
         <div class="poke-stats-info">
           <div class="row-stats">
             <div class="hovertext" data-hover="HP">
-              <i class="fa-solid fa-heart"> HP</i>
+              <i class="fa-solid fa-heart" id="poke-hp"> {{ hpStats }}</i>
             </div>
             <div class="hovertext" data-hover="Attack">
-              <i class="fa-solid fa-fire-flame-simple"> Atk</i>
+              <i class="fa-solid fa-fire-flame-simple" id="poke-atk"> {{ atkStats }}</i>
             </div>
           </div>
           <div class="row-stats">
             <div class="hovertext" data-hover="Defense">
-              <i class="fa-solid fa-shield-halved"> Def</i>
+              <i class="fa-solid fa-shield-halved" id="poke-def"> {{ defStats }}</i>
             </div>
             <div class="hovertext" data-hover="Special Attack">
-              <i class="fa-solid fa-fire"> SAtk</i>
+              <i class="fa-solid fa-fire" id="poke-satk"> {{ satkStats }}</i>
             </div>
           </div>
           <div class="row-stats">
             <div class="hovertext" data-hover="Special Defense">
-              <i class="fa-solid fa-shield"> SDef</i>
+              <i class="fa-solid fa-shield" id="poke-sdef"> {{ sdefStats }}</i>
             </div>
             <div class="hovertext" data-hover="Speed">
-              <i class="fa-solid fa-wind"> Spd</i>
+              <i class="fa-solid fa-wind" id="poke-spd"> {{ spdStats }}</i>
             </div>
+            
           </div>
         </div>
       </div>
@@ -67,7 +109,8 @@ main{
 }
 .container{
   background-color: blueviolet;
-  width: 25%;
+  width: 90%;
+  max-width: 450px;
   /* height: 45%; */
   aspect-ratio: 1;
   border-radius: 5%;
@@ -112,7 +155,7 @@ main{
   border-radius: 1em;
 }
 .hide{
-  display: none;
+  display: none !important;
 }
 .poke-data{
   display: flex;
@@ -140,12 +183,10 @@ main{
   justify-content: center;
   padding: 1em;
 }
-.dot {
-  height: 10em;
+.poke-img{
   width: 10em;
-  background-color: #bbb;
   border-radius: 50%;
-  display: inline-block;
+  border: .1em dashed black;
 }
 .poke-stats-info{
   height: 100%;
